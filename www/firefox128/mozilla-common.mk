ca3cfc3d631a05a10da42a9fbf1d218a34bc544f
Erik Müller <e-nospam@mueller-ramberg.de>
2024-10-12 23:28:57 +0200

    Disable unavailable webassembly functionality.

--- www/firefox128/mozilla-common.mk
+++ www/firefox128/mozilla-common.mk
@@ -120,7 +120,7 @@ CONFIGURE_ARGS+=	--with-clang-path=${PREFIX}/bin/clang
 CONFIGURE_ARGS+=	--with-libclang-path=${PREFIX}/lib
 
 # RLBox WASM sandbox
-.if ${MACHINE_ARCH} == "x86_64" || ${MACHINE_ARCH} == "i386" || ${MACHINE_ARCH} == "powerpc64le"
+.if ${MACHINE_ARCH} == "x86_64" || ${MACHINE_ARCH} == "i386"
 # For wasm-ld command
 TOOL_DEPENDS+=		lld-[0-9]*:../../devel/lld
 .include "../../lang/wasi-libc/buildlink3.mk"
